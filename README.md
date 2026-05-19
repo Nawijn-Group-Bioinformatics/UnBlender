aed3f3f350fa42c9a0b7d662d58c6ecf2b3cc64d
Tessa Gillett <tessagillett@hotmail.com>
2026-05-19 17:21:33 +0200

    Add overview cell type labels, handle non-alphanumeric labels, minor other updates

--- README.md
+++ README.md
@@ -1,20 +1,30 @@
-UnBlender allows respiratory scientists to perform cell type deconvolution in a validated manner. Not all cell type deconvolution approaches are feasible, especially when using highly granular (i.e. high resolution, very specific) cell type labels. UnBlender leverages the Human Lung Cell Atlas reference data [(Sikkema et al., 2023)](https://www.nature.com/articles/s41591-023-02327-2) to allow the user to specify a cell type label granularity that suits the needs of their research question, and then validates whether that approach is feasible.
+UnBlender allows respiratory scientists to perform cell type deconvolution in a validated manner. Not all cell type deconvolution approaches are feasible, especially when using highly granular (i.e. high resolution, very specific) cell type labels. UnBlender leverages the Human Lung Cell Atlas reference data [(Sikkema et al., 2023)](https://www.nature.com/articles/s41591-023-02327-2) to allow the user to specify a cell type label granularity that suits the needs of their research question, and then validates whether that approach is able to yield accurate results.
 
-(Why is this important? Because otherwise you run a very realistic risk of generating meaningless deconvolution results. For reliable results, it is essential to test whether the chosen combination of cell types *can* be deconvoluted in the chosen sample type.)
+Why is this important? Because otherwise there is a very realistic risk of generating meaningless deconvolution results. For reliable results, it is essential to test whether the chosen combination of cell types *can* accurately be deconvoluted in the chosen sample type.
 
-### Webtool
-UnBlender is available as an online webtool at www.unblender.io, which can be used for deconvolution of nasal brush, bronchial brush, bronchial biopsy and parenchymal resection samples. No installation of software required, with a clickable user interface to configure analysis parameters.
+How does UnBlender evaluate deconvolution accuracy? In short: by deconvoluting pseudo-bulk samples with a known cell type composition. More information can be found in [TODO].
+
+### Graphical user interface
+UnBlender will soon be available as an easy to use graphical user interface (GUI), with a point-and-click menus to configure analysis parameters. The UnBlender GUI evaluates deconvolution accuracy of the specified approach, and can then be used to deconvolute your own transcriptomics dataset. The GUI can be used for deconvolution of nasal brush, bronchial brush, bronchial biopsy and parenchymal resection samples.
+
+Output of the UnBlender GUI:
+- A visual summary of expected deconvolution accuracy per cell type
+- A table of deconvolution accuracy evaluation results
+- Deconvolution analysis results on your transcriptomics dataset
+
+Want to try it now? A beta version of the GUI with full functionality is available for testers, contact us at unblender.info@gmail.com or submit an issue to this repository.
 
 ### Command line interface
-However, if you wish to deconvolute your bulk RNA-seq dataset on your own computer, this GitHub repository provides a pipeline for the command line interface (CLI).
+For those who prefer command line functionality, this GitHub repository provides a pipeline for the command line interface (CLI). The UnBlender CLI evaluates a specified deconvolution approach in the selected sample type, and returns the following:
 
-Output of the CLI pipeline:
-- A signature matrix that you can use to perform cell type deconvolution
-- A per-cell type evaluation whether deconvolution results using this signature matrix are reliable in your sample type
+- A summary of the expected deconvolution accuracy
+- A signature matrix that can be used to deconvolute your transcriptomics dataset
 - The reference data used to generate the signature matrix
 
-## How to use:
-1) Have CIBERSORTx (docker) installed, and activate it.
+Then, the user can take the signature matrix (or reference data) and perform cell type deconvolution analysis on their dataset. The signature matrix can be saved and shared for future analyses, and easily be incorporated into an existing deconvolution workflow using your tool of preference. 
+
+#### How to use the UnBlender CLI:
+1) Install CIBERSORTx (docker), and activate it.
 2) Install & activate the conda environment supplied with the code.
 3) Configure your pipeline run using a config.yaml file (example file is provided, see below for parameters)
 4) [Download the .h5ad HLCA file (core).](https://cellxgene.cziscience.com/collections/6f6d381a-7701-4781-935c-db10d30de293)
@@ -22,11 +32,11 @@ Output of the CLI pipeline:
 5) Run the pipeline using the following command: 
 	snakemake -c1 --configfile [config_file_name.yaml]
 
-## Configuration of parameters:
+#### Configuration of parameters:
 - config_filename: the absolute path + file name to the config.yaml file, e.g. /home/user/Documents/my_deconvolution_analysis/config.yaml
 - sample_type: "parenchyma", "bronchial_brush", "nasal_brush", or "bronchial_biopsy"
 - output_dir: absolute path to the output directory, e.g.  /home/user/Documents/my_deconvolution_analysis/output
-- DEG_file: optional, the absolute path + file name to a file with any gene names you want to remove from the analysis (e.g. because they are differentially expressed in your bulk RNA-seq cohort). If you don't want to use this feature, give it value "no_filter". It is *not* recommended to remove large numbers of genes, as this will reduce deconvolution accuracy. However, in specific cases it may be worthwhile to remove a few. Format: .csv/txt/tsv etc. file with a single gene identifier (HGNC gene names) on each new line.
+- DEG_file: optional, the absolute path + file name to a file with any gene names you want to remove from the analysis (e.g. because they are differentially expressed in your bulk RNA-seq cohort). If you don't want to use this feature, give it value "no_filter". It is *not* recommended to remove large numbers of genes, as this will reduce deconvolution accuracy. However, in specific cases it may be worthwhile to remove a few. Format: .csv/.txt/.tsv file with a single gene identifier (HGNC gene names) on each new line.
 - HLCA_file: path to the .h5ad HLCA file.
 - email: the email address with which you run CIBERSORTx
 - token: your private CIBERSORTx token
@@ -45,6 +55,9 @@ Output of the CLI pipeline:
     - "Multiciliated lineage"
 ```
 
+# Citing UnBlender
+Have you used UnBlender (GUI or CLI) to perform a cell type deconvolution analysis? Please cite us as: [TODO]
+
 # UnBlender is in beta version
-UnBlender pipeline code is in beta version. If you encounter any bugs, please contact unblender.info@gmail.com.
+UnBlender pipeline code is in beta version. If you encounter any bugs, please submit an issue to this repository. Or contact unblender.info@gmail.com.
 (A known bug is that currently, use of certain cell types with uncommon non-alphanumeric characters in their name may cause problems running the pipeline.)
--- source/process_deconv_outputs.R
+++ source/process_deconv_outputs.R
@@ -9,7 +9,7 @@ library(tidyr)
 library(ggpubr)
 library(dplyr)
 library(RColorBrewer)
-library(ggrepel)
+# library(ggrepel)
 library(cowplot)
 
 # Setup
@@ -33,12 +33,23 @@ sigmat_file = args[6]
 
 
 # 
-# # ARGS for testing:
+# # ARGS for testing PARENCHYMA:
 # cell_counts_file = "/home/tessa/Documents/20230221_Signature_matrix_validation_webtool/manuscript_analyses/output/SUBANALYSIS_corrected_or_not/parenchyma_uncorrected/cell_counts.csv"
 # deconv_results_file = "/home/tessa/Documents/20230221_Signature_matrix_validation_webtool/manuscript_analyses/output/SUBANALYSIS_corrected_or_not/parenchyma_uncorrected/CIBERSORTx_output/CIBERSORTx_Results.txt"
 # output_dir = "/home/tessa/Desktop/TEMP/"
 # correct=F
 # ref_counts_file = "/home/tessa/Documents/20230221_Signature_matrix_validation_webtool/manuscript_analyses/output/SUBANALYSIS_corrected_or_not/parenchyma_uncorrected/nasal_brush_subsampled_matrix_max200cells_HUGO.txt"
+# # NEW: ADD TO CONFIG!!! AND SETUP!!
+# sigmat_file = '/home/tessa/Documents/20230221_Signature_matrix_validation_webtool/manuscript_analyses/output/SUBANALYSIS_corrected_or_not/parenchyma_uncorrected/nasal_brush/CIBERSORTx_output/CIBERSORTx_nasal_brush_subsampled_matrix_max200cells_HUGO_inferred_phenoclasses.CIBERSORTx_nasal_brush_subsampled_matrix_max200cells_HUGO_inferred_refsample.bm.K999.txt'
+# correct_sigmat_only = F
+# 
+# 
+# # ARGS for testing NASAL:
+# cell_counts_file = "/home/tessa/Documents/20230221_Signature_matrix_validation_webtool/manuscript_analyses/output/SUBANALYSIS_corrected_or_not/nasal_uncorrected/cell_counts.csv"
+# deconv_results_file = "/home/tessa/Documents/20230221_Signature_matrix_validation_webtool/manuscript_analyses/output/SUBANALYSIS_corrected_or_not/nasal_uncorrected/CIBERSORTx_output/CIBERSORTx_Results.txt"
+# output_dir = "/home/tessa/Desktop/TEMP/"
+# correct=F
+# ref_counts_file = "/home/tessa/Documents/20230221_Signature_matrix_validation_webtool/manuscript_analyses/output/SUBANALYSIS_corrected_or_not/nasal_uncorrected/nasal_brush_subsampled_matrix_max200cells_HUGO.txt"
 # # NEW: ADD TO CONFIG!!! AND SETUP!!
 # sigmat_file = '/home/tessa/Documents/20230221_Signature_matrix_validation_webtool/manuscript_analyses/output/SUBANALYSIS_corrected_or_not/parenchyma_uncorrected/nasal_brush/CIBERSORTx_output/CIBERSORTx_nasal_brush_subsampled_matrix_max200cells_HUGO_inferred_phenoclasses.CIBERSORTx_nasal_brush_subsampled_matrix_max200cells_HUGO_inferred_refsample.bm.K999.txt'
 # correct_sigmat_only = F
@@ -66,10 +77,23 @@ deconv$sample = rownames(deconv)
 deconv_plottable <- pivot_longer(deconv, 1:num_columns, names_to = "custom_labels", values_to = "sample_proportions")
 # result: deconv_plottable --> tibble with columns: P.value, Correlation, RMSE, sample, custom_labels, sample_proportions
 
+# Match labels to ground truth formatting
+deconv_plottable$custom_labels <- gsub("\\.\\.\\.", " & ", deconv_plottable$custom_labels) 
+deconv_plottable$custom_labels <- gsub("\\.", " ", deconv_plottable$custom_labels) 
+deconv_plottable$custom_labels <- gsub("Hillock like", "Hillock-like", deconv_plottable$custom_labels)
+deconv_plottable$custom_labels <- gsub("CCL3 ", "CCL3+", deconv_plottable$custom_labels)
+deconv_plottable$custom_labels <- gsub(" non-nasal ", "(non-nasal)", deconv_plottable$custom_labels)
+deconv_plottable$custom_labels <- gsub(" nasal ", "(nasal)", deconv_plottable$custom_labels)
+deconv_plottable$custom_labels <- gsub(" bronchial ", "(bronchial)", deconv_plottable$custom_labels)
+deconv_plottable$custom_labels <- gsub(" subsegmental ", "(subsegmental)", deconv_plottable$custom_labels)
+deconv_plottable$custom_labels <- gsub("MT positive", "MT-positive", deconv_plottable$custom_labels)
+deconv_plottable$custom_labels <- gsub("Monocyte derived", "Monocyte-derived", deconv_plottable$custom_labels)
+deconv_plottable$custom_labels <- gsub("pre TB", "pre-TB", deconv_plottable$custom_labels)
+deconv_plottable$custom_labels <- gsub("2 ", "2_", deconv_plottable$custom_labels)
+deconv_plottable$custom_labels <- gsub("4 ", "4_", deconv_plottable$custom_labels)
+deconv_plottable$custom_labels <- gsub("3 ", "3_", deconv_plottable$custom_labels)
+
 # Prepare to plot
-deconv_plottable$custom_labels <- gsub("\\.\\.\\.", " & ", deconv_plottable$custom_labels) # match labels to ground truth formatting
-deconv_plottable$custom_labels <- gsub("\\.", " ", deconv_plottable$custom_labels) # match labels to ground truth formatting
-deconv_plottable$custom_labels <- gsub("Hillock like", "Hillock-like", deconv_plottable$custom_labels) # match labels to ground truth formatting
 gtruth_withProportions$which_plot <- "Ground truth"
 deconv_plottable$which_plot <- "Deconvolution"
 names(gtruth_withProportions)[names(gtruth_withProportions) == "custom_label"] <- "custom_labels"
@@ -182,17 +206,17 @@ corr_plot <- ggplot(df_corr_results, aes(celltp, corr_test.estimate)) +
 gt_vs_est_plot <- ggplot(prop_error_df, aes(x=sample_proportions.x, y=sample_proportions.y)) +
   geom_abline(slope=1, intercept = 0, color='chartreuse3') +
   geom_point() + facet_wrap(~custom_labels.x, scales='free') +
-  xlab("Ground truth: real sample proportions") + ylab("Deconvolution: estimated proportions") +
+  xlab("Ground truth: true sample proportions") + ylab("Deconvolution: estimated proportions") +
   ggtitle("True vs. estimated proportions (green line indicates values where estimated = true)")
 gt_vs_est_plot
 
-### Putting things together ###
-title <- ggdraw() + draw_label("Deconvolution accuracy per cell type")
+# Putting things together
+title <- ggdraw() + draw_label("Pseudobulk deconvolution accuracy per cell type")
 acc_plot <- plot_grid(title, plot_grid(MAPE_plot, corr_plot), rel_heights = c(0.1, 1), ncol=1)
 
-# write
+#### write output ####
 
-png(filename=paste0(output_dir, '/stacked_bar_chart.png'))
+png(filename=paste0(output_dir, '/pseudobulk_composition.png'))
 print(stacked_bars)
 dev.off()
 
@@ -203,3 +227,76 @@ dev.off()
 png(filename=paste0(output_dir, '/estimated_vs_true_proportions.png'), width=700, height=700)
 print(gt_vs_est_plot)
 dev.off()
+
+output_report = merge(MAPE_cell_type, df_corr_results, by.x='cell.type', by.y='celltp')
+colnames(output_report) = c('Cell type', 'MAPE', 'Correlation', 'Corr. p-value', 'Mean pseudobulk proportion', 'Stdev. pseudobulk proportions')
+write.table(output_report, sep=',', file = paste0(output_dir, '/deconvolution_accuracy_report.csv'), row.names = F)
+
+
+# #### For figure S1 biorXiv ####
+# 
+# 
+# 
+# # plot stacked bar chart
+# plot_proportions2 <- function(df, plot_title) {
+#   ggplot(df, aes(x = factor(sample) ,y = sample_proportions, fill = factor(custom_labels))) + 
+#     geom_bar(stat = "identity") +
+#     theme(strip.text = element_text(size=8),
+#           legend.key.size = unit(0.15, 'cm'), #change legend key size
+#           title = element_text(size=8), #change legend title font size
+#           legend.text = element_text(size=8), #change legend text font size
+#           axis.text.x = element_text(size=6, angle=45, vjust = 1, hjust=1)) +
+#     labs(title=plot_title, x ="Sample", y = "Cell type proportions", fill = " ") +
+#     facet_wrap(~which_plot, nrow =1) + scale_fill_manual(values = col_vector)
+# }
+# stacked_bars2 <- plot_proportions2(df_to_plot, paste("Pseudobulk composition"))
+# stacked_bars2
+# 
+# # MAPE
+# MAPE_plot2 <- ggplot(MAPE_cell_type, aes(cell.type, MAPE)) + 
+#   ylim(0,NA) + 
+#   annotate(geom='rect', ymin=-Inf, ymax=1, xmin = -Inf, xmax = Inf, 
+#            fill='darkolivegreen2', alpha=0.5) +
+#   annotate(geom='rect', ymin=1, ymax=Inf, xmin = -Inf, xmax = Inf, 
+#            fill='sandybrown', alpha=0.5) +
+#   geom_point(size=1) + coord_flip()+
+#   xlab("Cell type") + ylab("Mean absolute proportional error\nof estimated vs. true proportion") +
+#   theme(axis.title.x = element_text(size=8),
+#         axis.title.y = element_blank())
+# 
+# 
+# # Correlation per cell type
+# corr_plot2 <- ggplot(df_corr_results, aes(celltp, corr_test.estimate)) +
+#   ylim(0,NA) + 
+#   annotate(geom='rect', ymin=0.7, ymax=Inf, xmin = -Inf, xmax = Inf, 
+#            fill='darkolivegreen2', alpha=0.5) +
+#   annotate(geom='rect', ymin=-Inf, ymax=0.7, xmin = -Inf, xmax = Inf, 
+#            fill='sandybrown', alpha=0.5) + coord_flip()+
+#   geom_point(size=1) +
+#   xlab("Cell type") + ylab("Correlation of estimated vs.\ntrue cell type proportions") +
+#   theme(axis.title.x = element_text(size=8),
+#         axis.title.y = element_blank(),
+#         axis.text.y = element_blank()
+#         )
+# 
+# # Ground truth vs estimated % plot
+# gt_vs_est_plot2 <- ggplot(prop_error_df, aes(x=sample_proportions.x, y=sample_proportions.y)) +
+#   geom_abline(slope=1, intercept = 0, color='chartreuse3') +
+#   geom_point(size=0.8) + facet_wrap(~custom_labels.x, scales='free') +
+#   xlab("Ground truth: true sample proportions") + ylab("Deconvolution: estimated proportions") +
+#   ggtitle("True vs. estimated proportions\n(Green line indicates values where estimated equals true)") +
+#   theme(strip.text = element_text(size=8),
+#         title = element_text(size=8),
+#         axis.text.x = element_text(size=7),
+#         axis.text.y = element_text(size=7))
+# gt_vs_est_plot2
+# 
+# # Putting accuracy plot together
+# title <- ggdraw() + draw_label("Pseudobulk deconvolution accuracy per cell type", size = 10)
+# acc_plot2 <- plot_grid(title, 
+#                        plot_grid(NULL, MAPE_plot2, corr_plot2, NULL, rel_widths = c(0.2,2,1.1, 0.2), nrow=1), 
+#                        rel_heights = c(0.1, 1), ncol=1)
+# 
+# 
+# plot_grid(plot_grid(NULL, stacked_bars2, ncol=2, rel_widths = c(0.1,1)), acc_plot2, gt_vs_est_plot2, ncol=1, rel_heights = c(1.2,0.8,2),
+#           labels = LETTERS[1:3])
